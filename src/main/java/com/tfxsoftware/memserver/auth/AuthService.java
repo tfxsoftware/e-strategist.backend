291d53ac16744d711c8140394e92a9a1b7bb573e
tfxsoftware <aswespeak.tfx@gmail.com>
2026-01-30 15:37:59 -0300

    feat(auth): implement signIn method for user authentication

--- src/main/java/com/tfxsoftware/memserver/auth/AuthService.java
+++ src/main/java/com/tfxsoftware/memserver/auth/AuthService.java
@@ -5,10 +5,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.tfxsoftware.memserver.auth.dto.SignInResponse;
 import com.tfxsoftware.memserver.auth.dto.SignUpDto;
-import com.tfxsoftware.memserver.users.User;
+import com.tfxsoftware.memserver.auth.dto.SignInDto;
 import com.tfxsoftware.memserver.users.UserService;
 import com.tfxsoftware.memserver.users.dto.CreateUserDto;
+import com.tfxsoftware.memserver.users.User;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -42,5 +44,23 @@ public class AuthService {
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error!");
         }
+    }
+
+    public SignInResponse signIn(SignInDto dto){
+        String email = dto.getEmail().trim().toLowerCase();
+    
+        User user;
+
+        try {
+            user = userService.getUserByEmail(email);
+        } catch (ResponseStatusException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
+        }
+
+        if (!passwordEncoder.matches(dto.getPassword(), user.getHashedPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
+        }
+
+        return new SignInResponse("teste");
     }
 }
