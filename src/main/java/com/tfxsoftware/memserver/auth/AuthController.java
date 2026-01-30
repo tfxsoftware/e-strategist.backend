609710a46b64eae978253cbd5020aa9be3f8f145
tfxsoftware <aswespeak.tfx@gmail.com>
2026-01-30 15:37:56 -0300

    refactor(auth): update register method to return 201 Created and remove SignUpResponse

--- src/main/java/com/tfxsoftware/memserver/auth/AuthController.java
+++ src/main/java/com/tfxsoftware/memserver/auth/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tfxsoftware.memserver.auth.dto.SignInDto;
+import com.tfxsoftware.memserver.auth.dto.SignInResponse;
 import com.tfxsoftware.memserver.auth.dto.SignUpDto;
 
 import jakarta.validation.Valid;
@@ -19,8 +21,15 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> register(@RequestBody @Valid SignUpDto dto) {
-        SignUpResponse response = authService.register(dto);
+    public ResponseEntity<Void> register(@RequestBody @Valid SignUpDto dto) {
+        authService.signUp(dto);
+        // Use 201 Created for successful signups
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<SignInResponse> signIn(@RequestBody @Valid SignInDto dto) {
+        SignInResponse response = authService.signIn(dto);
         // Use 201 Created for successful signups
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
